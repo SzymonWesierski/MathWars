6c391d9ceaf10563ba10c50f4bb3b55dc995db05
Szymon Węsierski <s.wesierski.151@studms.ug.edu.pl>
2023-11-09 19:36:54 +0100

    fix: task validation

--- MathWars/Pages/TaskPages/CreateTask.cshtml.cs
+++ MathWars/Pages/TaskPages/CreateTask.cshtml.cs
@@ -3,31 +3,25 @@ using MathWars.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace MathWars.Pages.TaskPages
+namespace MathWars.Pages.TaskPages;
+[BindProperties]
+public class CreateTaskModel : PageModel
 {
-    public class CreateTaskModel : PageModel
+    private readonly ApplicationDbContext _db;
+    public Tasks Task { get; set; }
+
+    public CreateTaskModel(ApplicationDbContext db)
     {
-        private readonly ApplicationDbContext _db;
-        [BindProperty]
-        public Tasks Task { get; set; }
+        _db = db;
+    }
+    public void OnGet()
+    {
+    }
 
-        public CreateTaskModel(ApplicationDbContext db)
-        {
-            _db = db;
-        }
-        public void OnGet()
-        {
-        }
-
-        public async Task<IActionResult> OnPost()
-        {
-            if (ModelState.IsValid)
-            {
-                await _db.Tasks.AddAsync(Task);
-                await _db.SaveChangesAsync();
-                return RedirectToPage("ViewTasks");
-            }
-            return Page();
-        }
+    public async Task<IActionResult> OnPost()
+    {
+            await _db.Tasks.AddAsync(Task);
+            await _db.SaveChangesAsync();
+            return RedirectToPage("ViewTasks");
     }
 }
