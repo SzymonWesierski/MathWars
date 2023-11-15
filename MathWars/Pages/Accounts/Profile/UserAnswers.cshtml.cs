4d56270a51a405190a7d075207edb4f6389df4d4
Szymon Węsierski <s.wesierski.151@studms.ug.edu.pl>
2023-11-15 20:22:03 +0100

    feat: Your Answers Page

---
+++ MathWars/Pages/Accounts/Profile/UserAnswers.cshtml.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using MathWars.Data;
+using MathWars.Models;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MathWars.Pages
+{
+    [Authorize]
+    public class UserAnswersModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        private readonly ApplicationDbContext _db;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private ApplicationUser currentUser;
+        public IEnumerable<Answers> answers { get; set; }
+
+        public UserAnswersModel(ApplicationDbContext db, ILogger<IndexModel> logger, UserManager<ApplicationUser> userManager)
+        {
+            _db = db;
+            _logger = logger;
+            _userManager = userManager;
+        }
+
+        public async Task OnGet()
+        {
+            currentUser = await _userManager.GetUserAsync(User);
+			answers = _db.Answers.Include(a => a.Task).Where(a => a.UserId == currentUser.Id).ToList();
+		}
+    }
+}
